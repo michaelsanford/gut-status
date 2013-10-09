15745e9c7d2c77db7f70680d572959877f29d5e8
michaelsanford <michaelsanford0@gmail.com>
2013-10-09 15:07:16 -0400

    gut add

---
+++ gut.js
@@ -0,0 +1,42 @@
+if (process.argv[2] == 'status') {
+
+	var EOL = require('os').EOL;
+
+	var responses = [
+		"Kind of hungry.",
+		"Grumbling; try `$ gut add`",
+		"Just fine!",
+		"Ahead of master by ",
+		"Your branch is ahead of 'kitchen/master' by 2 meals.",
+		"Nothing to eat, working plate clean.",
+	];
+
+	var response = responses[Math.floor(Math.random() * responses.length)];
+
+	console.dir(response);
+
+} else if (process.argv[2] == 'add') {
+
+	console.log("Added " + process.argv[3]);
+
+} else if (process.argv[2] == 'pull') {
+
+    console.log("Forgot your " + process.argv[3] + " at home again?");
+
+} else if (process.argv[2] == 'push') {
+
+	console.log("Eating " + process.argv[3]);
+
+} else if (process.argv[2] == 'clone' || process.argv[2] == 'fetch') {
+
+	console.log("It's not nice to steal " + process.argv[3] + "from the break-room repository...");
+
+} else if (process.argv[2] == 'merge') {
+
+	console.log ("Merging in ")
+
+} else {
+
+	console.log("That verb is not currently supported.");
+
+}
